17f7e75f9bf682262f1f105053effd591ca10eae
agcaiyun <agcaiyun@163.com>
2018-02-26 23:57:17 +0800

    refactor filter choose_even.js

--- 第一期训练营/task4-collection-calculate-camp/practices/filter/choose_even.js
+++ 第一期训练营/task4-collection-calculate-camp/practices/filter/choose_even.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function choose_even(collection) {
-  //filter 不会改变元素组,所以不需要 Array.from(collection)
-  return collection.filter((item) => item % 2 === 0);
+  return collection.filter(item => item % 2 === 0);
 }
 
 module.exports = choose_even;
