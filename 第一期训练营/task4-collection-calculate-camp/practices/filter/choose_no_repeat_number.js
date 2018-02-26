6de3e92f5bb9058ce4c3d4495828f6153649bbc7
agcaiyun <agcaiyun@163.com>
2018-02-27 00:26:39 +0800

    refactor filter choose_no_repeat_number.js

--- 第一期训练营/task4-collection-calculate-camp/practices/filter/choose_no_repeat_number.js
+++ 第一期训练营/task4-collection-calculate-camp/practices/filter/choose_no_repeat_number.js
@@ -1,18 +1,15 @@
 'use strict';
-function choose_no_repeat_number(collection) {
-  let result = [];
-  for (let item of collection) {
-    result.includes(item) ? null : result.push(item);
-  }
-  return result;
-}
-
 // function choose_no_repeat_number(collection) {
-//   let result = new Set();
-//   for (let item of collection) {
-//     result.add(item);
-//   }
-//   return Array.from(result);
+//   let result = []
+//   return collection.filter((ele) => {
+//     return !result.includes(ele) ? result.push(ele) : null
+//   })
 // }
+
+function choose_no_repeat_number(collection) {
+  let result = new Set();
+  collection.map(item => result.add(item))
+  return Array.from(result);
+}
 
 module.exports = choose_no_repeat_number;
